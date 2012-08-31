dc0809c0abb890369a5384621c2b281986c35397
Kristopher Cost <kristopher.cost@demandmedia.com>
2012-08-31 00:08:09 -0700

    master: adding customizable background color

--- fpo.go
+++ fpo.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 	"net/http"
 	"image"
 	"image/draw"
@@ -9,35 +9,41 @@ import (
 	"image/png"
 	"regexp"
 	"strconv"
+	"encoding/hex"
 )
 
 type ImageHandler struct{}
 
 var (
-	dimensions_regex, err = regexp.Compile(`/([0-9]+)x([0-9]+)$`)
+	request_regex, _ = regexp.Compile(`/([0-9]+)x([0-9]+)/*([0-9a-f]{6}){0,1}`)
 	gray = color.RGBA{240, 240, 240, 255}
 	canvas = &image.Uniform{gray}
 )
 
 func (h ImageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// fmt.Printf("request path: %v ", r.URL.Path)
+	// fmt.Printf("request path: %v \n", r.URL.Path)
 
-	if err != nil {
+	options := request_regex.FindAllStringSubmatch(r.URL.Path, -1)
+
+	if options == nil {
+		// fmt.Print("options is nil, aborting.\n")
 		http.Error(w, "", 400)
 		return
 	}
 
-	dims := dimensions_regex.FindAllStringSubmatch(r.URL.Path, -1)
+	height, err := strconv.Atoi(options[0][1])
+	width, err := strconv.Atoi(options[0][2])
+	colorhex := options[0][3]
 
-	if (dims == nil) {
-		http.Error(w, "", 400)
-		return
+	fmt.Printf("image request: %v, %v, %v\n", height, width, colorhex)
+
+	// fmt.Printf("length: %d\n", len(colorhex))
+
+	if len(colorhex) > 0 {
+		red, green, blue := get_colors(colorhex)
+		// fmt.Printf("%v, %v, %v\n", red, green, blue)
+		canvas.C = color.RGBA{red, green, blue, 255}
 	}
-
-	height, err := strconv.Atoi(dims[0][1])
-	width, err := strconv.Atoi(dims[0][2])
-
-	// fmt.Printf("request for image, h: %v, w: %v\n", height, width)
 
 	if err != nil {
 		http.Error(w, "", 400)
@@ -51,6 +57,15 @@ func (h ImageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Cache-control", "public, max-age=259200")
 
 	png.Encode(w, m)
+	canvas.C = gray
+}
+
+func get_colors(colorhex string) (byte, byte, byte) {
+	red, _ := hex.DecodeString(colorhex[0:2])
+	green, _ := hex.DecodeString(colorhex[2:4])
+	blue, _ := hex.DecodeString(colorhex[4:])
+
+	return red[0], green[0], blue[0]
 }
 
 func main() {
